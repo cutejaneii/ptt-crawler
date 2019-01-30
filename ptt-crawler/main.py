979fb45b0fcf9554cef21174a6ac32a5349c89b7
Jennifer <cutejaneii@hotmail.com>
2019-01-30 23:35:25 +0800

    Update main.py

--- ptt-crawler/main.py
+++ ptt-crawler/main.py
@@ -1,62 +1,49 @@
 # -*- coding: utf-8 -*-
 from model import ptt_article_model
 from ptt_crawler import ptt_crawl, ptt_crawl_by_pageno
-import sys
-import uuid
+from argparse import ArgumentParser
 
 if __name__ == '__main__':
-    #text_file = open('gossip.txt', 'w')
-    results = []
+    parser = ArgumentParser()
+    parser.add_argument('-b', '--board', default='movie', help='ptt board name (default: movie)')
+    parser.add_argument('-a', '--article_id', default='', help='Article id')
+    parser.add_argument('-m', '--mode', type=int, default=0, help='Query mode (default:0)  0: The NEWEST articles, 1:Articles from pageno to pageno, 2:Certain page, 3:get articles by KEYWORD ')
+    parser.add_argument('-c', '--count', type=int, default=10, help='Article count (default: 10)')
+    parser.add_argument('-f', '--from_pageno', type=int, default=1, help='Article count (default: 1)')
+    parser.add_argument('-t', '--to_pageno', type=int, default=1, help='Article count (default: 1)')
+    parser.add_argument('-r', '--get_responses', type=int, default=0, help='Get response (default:0)  0:No responses, 1:With response')
+    parser.add_argument('-k', '--keyword', default='pizza', help='Keyword')
+    args = parser.parse_args()
+    
+    data = []
     newest_article_id=''
+    check=True
 
-    board='Gossiping'
-    article_id='-----'
-    previous_day=0
-    crawl_mode=0 # crawl from pageno to pageno
-    from_pageno = 1
-    to_pageno = 3
+    if (args.mode not in [0, 1, 2, 3]):
+        print('[mode] can only be 0, 1, 2, 3')
+        check=False
 
-    try:
-        crawl_mode=int(sys.argv[1])
-    except:
-        pass
+    if (args.get_responses not in [0, 1]):
+        print('[get_responses] can only be 0, 1.')
+        check=False
 
-    if (crawl_mode==0):
-        try:
-            from_pageno = int(sys.argv[2])
-            to_pageno = int(sys.argv[3])
-        except:
+
+    if (args.count == 0):
+        print('[count] should bigger than 0.')
+        check=False
+
+    if (check==True):
+        if (args.mode==0):
             pass
-    else:
-        try:
-            board=sys.argv[2]
-        except:
-            pass
-        try:
-            article_id=sys.argv[3]
-        except:
-            pass
-        try:
-            previous_day=int(sys.argv[4])
-        except:
-            pass
+        else:
+            if (args.mode==1):
+                for x in range(args.from_pageno, (args.to_pageno+1), 1):
+                    print('<------------------ Crawling data, pageno='+str(x) + '------------------>')
+                    newest_article_id, results = ptt_crawl_by_pageno(board=args.board,pageno=args.from_pageno)
+            elif (args.mode==2):
+                newest_article_id, data = ptt_crawl_by_pageno(board=args.board,pageno=args.from_pageno)
+            else:
+                pass
 
-
-    if (crawl_mode==0):
-        print('Start to crawl from page:'+ str(from_pageno) + ' to page:'+str(to_pageno))
-        for x in range(from_pageno, (to_pageno+1), 1):
-            print('<------------------ Crawling data, pageno='+str(x) + '------------------>')
-            newest_article_id, results = ptt_crawl_by_pageno(board=board,pageno=x)
-            save_crawl_data(results)
-    else:
-        newest_article_id, results = ptt_crawl(board=board, keyword='', previous_day_count=previous_day,  last_aritlce_id=article_id)
-
-    print('newest_article_id:' + newest_article_id)
-
-    #while pageno > 0 :
-    #    pageno, results = ptt_crawl('gossiping','', pageno)
-    #    for i in results:
-    #        for j in i:
-    #            print(j)
-    #            text_file.write(j+'\n')
-#text_file.close()
+        for article in data:
+            print(article.title)
