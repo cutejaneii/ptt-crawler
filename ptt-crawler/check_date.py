7a52355b1f88d5821a4c2e7a36e1bff7a3ac4293
Jennifer <cutejaneii@hotmail.com>
2019-02-01 16:25:28 +0800

    Update check_date.py

--- ptt-crawler/check_date.py
+++ ptt-crawler/check_date.py
@@ -1,10 +1,9 @@
 from datetime import datetime, timedelta
 
-
-def check_ptt_date(ptt_date_string, before_days_count):
+def in_days(ptt_date_string, before_days_count):
   #ptt_date_string='Wed Jan 14 09:36:59 2019'
 
-  ptt_dt = ptt_date_string.split(' ')
+  ptt_dt = ptt_date_string.replace('  ', ' ').split(' ')
 
   i_month=0
   i_year=int(ptt_dt[4])
@@ -35,16 +34,13 @@ def check_ptt_date(ptt_date_string, before_days_count):
   if (ptt_dt[1]=='Dec'):
     i_month=12
 
-
   ptt_date = datetime(i_year, i_month, i_day)
-
   today = datetime(datetime.today().year, datetime.today().month, datetime.today().day)
-
   dt = today.date() - ptt_date.date()
 
   is_check=False
 
-  if (dt.total_seconds() > (before_days_count*24*60*60)):
+  if (dt.total_seconds() < (before_days_count*24*60*60)):
     is_check=True
 
   return is_check
