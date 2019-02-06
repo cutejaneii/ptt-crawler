cc9bc82ef57a781987088dfbcba47cd3b47dd98b
Jennifer <cutejaneii@hotmail.com>
2019-02-06 23:42:00 +0800

    Update main.py

--- ptt-crawler/main.py
+++ ptt-crawler/main.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 from model import ptt_article_model
-from ptt_crawler import ptt_crawl, crawl_by_single_page, crawl_by_pages, ptt_crawl_by_days
+from ptt_crawler import ptt_crawl, crawl_by_single_page, crawl_by_pages
 from argparse import ArgumentParser
 import datetime
+
 
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('-b', '--board', default='movie', help='ptt board name (default: movie)')
     parser.add_argument('-a', '--article_id', default='**', help='Article id')
-    parser.add_argument('-m', '--mode', type=int, default=0, help='Query mode (default:0)  0: The NEWEST articles, 1:Articles from pageno to pageno, 2:Certain page, 3:get articles by KEYWORD ')
+    parser.add_argument('-m', '--mode', type=int, default=0, help='Query mode (default:0)  0: The NEWEST articles, 1:Articles from pageno to pageno, 2:Articles from certain page')
     parser.add_argument('-c', '--count', type=int, default=10, help='Article count (default: 10)')
     parser.add_argument('-f', '--from_pageno', type=int, default=1, help='Article count (default: 1)')
     parser.add_argument('-t', '--to_pageno', type=int, default=1, help='Article count (default: 1)')
@@ -19,8 +20,8 @@ if __name__ == '__main__':
     data = []
     check=True
 
-    if (args.mode not in [0, 1, 2, 3]):
-        print('[mode] can only be 0, 1, 2, 3')
+    if (args.mode not in [0, 1, 2]):
+        print('[mode] can only be 0, 1, 2')
         check=False
 
     if (args.get_responses not in [0, 1]):
@@ -39,15 +40,13 @@ if __name__ == '__main__':
         else:
             if (args.mode==1):
                 data = crawl_by_pages(board=args.board,from_pageno=args.from_pageno, to_pageno=args.to_pageno,is_get_responses = args.get_responses)
-            elif (args.mode==2):
-                data = crawl_by_single_page(board=args.board,pageno=args.from_pageno, is_get_responses = args.get_responses)
             else:
-                data = ptt_crawl_by_days(board=args.board, previous_day_count = args.days, is_get_responses = args.get_responses)
-
-        #print('start='+s)
-        #print('end='+str(datetime.datetime.now()))
+                data = crawl_by_single_page(board=args.board,pageno=args.from_pageno, is_get_responses = args.get_responses)
 
         for article in data:
             print(article.title)
             for i in article.responses:
                 print('  ' + i.push_tag + i.content)
+        print('from page['+ str(args.from_pageno) + '] to page[' + str(args.to_pageno) +']')
+        print('from ' + s)
+        print('to ' + str(datetime.datetime.now()))
